d27f83cd7f5159e7013df2d405003921baec879c
Mabvaru <mabvarukundai@gmail.com>
2023-07-18 18:46:21 +0200

    python 5 and 6

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,5 @@
-for i in range(10):
+#!/usr/bin/python3
+for i in range(1, 10):
     for j in range(i+1, 10):
-        print("{}{}, ".format(i, j), end='')
+        print("{:02d}, ".format(i*10+j), end='')
 print("\n")

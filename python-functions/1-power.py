57b4e5abe953bb7c28127f3b9e8955dd43b42f34
Mabvaru <mabvarukundai@gmail.com>
2023-07-22 01:32:59 +0200

    task 1

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,5 +1,9 @@
+#!/usr/bin/python3
 def pow(a, b):
-    result = 1
-    for _ in range(b):
-        result *= a
-    return result
+    if b >= 0:
+        result = 1
+        for _ in range(b):
+            result *= a
+        return result
+    else:
+        return 1 / pow(a, -b)

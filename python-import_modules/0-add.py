13b7c3a7e42ea43d7ab13bdd6f8114d6800ee415
Mabvaru <mabvarukundai@gmail.com>
2023-07-25 03:02:04 +0200

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -10,15 +10,16 @@ def add(a, b):
     """
     return a + b
 
-# Define variables a and b
-a = 1
-b = 2
+if __name__ == "__main__":
+    # Define variables a and b
+    a = 1
+    b = 2
 
-# Import the add function from add_0.py
-from add_0 import add
+    # Import the add function from add_0.py
+    from add_0 import add
 
-# Calculate the result using the add function
-result = add(a, b)
+    # Calculate the result using the add function
+    result = add(a, b)
 
-# Print the result using string formatting
-print("{} + {} = {}".format(a, b, result))
+    # Print the result using string formatting
+    print("{} + {} = {}".format(a, b, result))

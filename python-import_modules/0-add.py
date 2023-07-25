b3d8cf26ec8cacfee2133d91a9968ceaeecc0337
Mabvaru <mabvarukundai@gmail.com>
2023-07-25 02:51:14 +0200

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -6,17 +6,46 @@ def add(a, b):
         b: second integer
 
     Returns:
-        The return value. a + b
+        The return value. a - b
     """
-    return a + b
+    return a - b
 
-if __name__ == "__main__":
-    # Assign values to variables a and b
-    a = 1
-    b = 2
+# Define variables a and b
+a = 1
+b = 2
 
-    # Calculate the result using the add function
-    result = add(a, b)
+# Calculate the result using the add function and print the output
+result = add(a, b)
+print("a = {} and b = {} FAKE add() => a - b".format(a, b, result))
 
-    # Print the result using string formatting
-    print("{} + {} = {}".format(a, b, result))
+# Define variables a and b
+a = 10
+b = 30
+
+# Calculate the result using the add function and print the output
+result = add(a, b)
+print("a = {} and b = {} FAKE add() => a - b".format(a, b, result))
+
+# Define variables a and b
+a = -10
+b = 30
+
+# Calculate the result using the add function and print the output
+result = add(a, b)
+print("a = {} and b = {} FAKE add() => a - b".format(a, b, result))
+
+# Define variables a and b
+a = -10
+b = -30
+
+# Calculate the result using the add function and print the output
+result = add(a, b)
+print("a = {} and b = {} FAKE add() => a - b".format(a, b, result))
+
+# Define variables a and b
+a = 5
+b = "H"
+
+# Calculate the result using the add function and print the output
+result = add(a, b)
+print("a = {} and b = {} FAKE add() => a - b".format(a, b, result))

6e60d1b4e112019bfc53d8bde6ea13a2b5aac2a2
Mabvaru <mabvarukundai@gmail.com>
2023-07-25 03:25:09 +0200

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,3 +4,6 @@ a = 1
 b = 2
 
 print(f"{a} + {b} = {add_0.add(a, b)}")
+if __name__ == "__main__":
+    # Code that will only be executed when the file is run directly
+    print("This code will not be executed when the file is imported")

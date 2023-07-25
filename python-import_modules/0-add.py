96ca1b3b18f5ec187d73587808cd4350dfb07102
Mabvaru <mabvarukundai@gmail.com>
2023-07-25 03:43:50 +0200

    task

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,7 +4,4 @@ a = 1
 b = 2
 
 if __name__ == "__main__":
-    # Code that will only be executed when the file is run directly
-    pass
-
-print(f"{a} + {b} = {add_0.add(a, b)}")
+    print(f"{a} + {b} = {add_0.add(a, b)}")

59e6aaa0383fe3bfc652712d0fc245782720da6e
Mabvaru <mabvarukundai@gmail.com>
2023-07-25 03:33:44 +0200

    task 0

--- python-import_modules/add_0.py
+++ python-import_modules/add_0.py
@@ -9,3 +9,7 @@ def add(a, b):
         The return value. a + b
     """
     return (a + b)
+
+if __name__ == "__main__":
+    # Code that will only be executed when the file is run directly
+    print("This code will not be executed when the file is imported")

d57911ae9879b0f4652d2ac452b27d589fe54a4f
Mabvaru <mabvarukundai@gmail.com>
2023-07-25 03:22:51 +0200

    taks 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,13 +1,6 @@
-# Import the add function from add_0.py
-from add_0 import add
+import add_0
 
-if __name__ == "__main__":
-    # Define variables a and b
-    a = 1
-    b = 2
+a = 1
+b = 2
 
-    # Calculate the result using the add function
-    result = add(a, b)
-
-    # Print the result using string formatting
-    print("{} + {} = {}".format(a, b, result))
+print(f"{a} + {b} = {add_0.add(a, b)}")

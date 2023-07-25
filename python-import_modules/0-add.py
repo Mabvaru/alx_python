be6990ee513f0e5d86443556bdf7f3fd8ee917f3
Mabvaru <mabvarukundai@gmail.com>
2023-07-25 02:46:00 +0200

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,23 @@
-# Import the add function from add_0.py
-from add_0 import add
+def add(a, b):
+    """My addition function
 
-# Assign values to variables a and b
-a = 1
-b = 2
+    Args:
+        a: first integer
+        b: second integer
 
-# Calculate the result using the add function
-result = add(a, b)
+    Returns:
+        The return value. a + b
+    """
+    return a + b
 
-# Print the result using string formatting
-print("{} + {} = {}".format(a, b, result))
+# If the module is not being imported, execute the following code
+if __name__ == "__main__":
+    # Assign values to variables a and b
+    a = 1
+    b = 2
+
+    # Calculate the result using the add function
+    result = add(a, b)
+
+    # Print the result using string formatting
+    print("{} + {} = {}".format(a, b, result))

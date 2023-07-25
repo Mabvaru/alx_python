90f84ef59578174f44fb126a8520b08429dc04b0
Mabvaru <mabvarukundai@gmail.com>
2023-07-25 02:39:38 +0200

    task 0

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+# Assign values to variables a and b
+a = 1
+b = 2
+
+# Import the add function from add_0.py
+from add_0 import add
+
+# Calculate the result using the add function
+result = add(a, b)
+
+# Print the result using string formatting
+print("{} + {} = {}".format(a, b, result))
